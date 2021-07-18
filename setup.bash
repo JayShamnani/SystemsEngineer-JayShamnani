ded1b420c9a676993b9f159e669a677d2e71a2c7
shamnani.jay@gmail.com <shamnani.jay@gmail.com>
2021-07-19 00:25:33 +0530

    Logs removed from docker installation

--- setup.bash
+++ setup.bash
@@ -58,19 +58,21 @@ if [ $? -gt 0 ];
    echo "Installing Docker..."
    docker -v > /dev/null 2>&1
    # Installing Docker in system
-   while [ $? -gt 1 ]
+   while [ $? -gt 0 ]
       do 
          runInstall() {
-         docker -v > /dev/null 2>&1
-         curl -fsSL get.docker.com -o get-docker.sh
-         sudo sh get-docker.sh
-         sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-         sudo chmod +x /usr/local/bin/docker-compose
-         docker-compose > /dev/null 2>&1
-         return $?
+            docker -v > /dev/null 2>&1
+            curl -fsSL get.docker.com -o get-docker.sh
+            sudo sh get-docker.sh > /dev/null 2>&1
+            sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+            sudo chmod +x /usr/local/bin/docker-compose
+            docker-compose > /dev/null 2>&1
+            return $?
          }
-         if [ $? -gt 0 ]; then
-            runInstall()
+         docker -v > /dev/null 2>&1
+         if [ $? -gt 0 ]
+         then
+            runInstall
          else
          break;
          fi
