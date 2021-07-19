7bca9b787ae2600170fc6b72576d8066d6ea046a
shamnani.jay@gmail.com <shamnani.jay@gmail.com>
2021-07-19 13:32:59 +0530

    Environment Variables added

--- setup.bash
+++ setup.bash
@@ -50,65 +50,118 @@ domain="example.com"
 
 # check if docker is installed or not
 docker -v > /dev/null 2>&1
+export IS_DOCKER_PRESENT=$?
 
-if [ $? -gt 0 ]; 
+docker-compose > /dev/null 2>&1
+export IS_DOCKER_COMPOSE_PRESENT=$?
+
+if [ $IS_DOCKER_PRESENT -ge 1 ]; 
 
    then
-   echo "Looks like docker is not installed in this system"
-   echo "Installing Docker..."
+   if [ $DOCKER_INSTALL_RESULT -ge 1 ] > /dev/null 2>&1
    
-   docker -v > /dev/null 2>&1
+   then
    
-   # Installing Docker in system
-   if [ $? -gt 0 ]
-      then
-         docker -v > /dev/null 2>&1
-         curl -fsSL get.docker.com -o get-docker.sh
-         sudo sh get-docker.sh > /dev/null 2>&1
-         sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose > /dev/null 2>&1
-         sudo chmod +x /usr/local/bin/docker-compose > /dev/null 2>&1
-         docker-compose > /dev/null 2>&1
-   fi
+      echo "Tried to install docker but failed durning installation."
+      echo "Trying Installing docker and docker compose manually."
    
-   # If all the setup are done then the further script will execute
-   ./setup.bash
-
    else
 
-         if [ $# -lt 1 ]
-            then
-            echo "$usage"
+      echo "Looks like docker is not installed in this system"
+      echo "Installing Docker..."
+      
+      # Installing Docker
+      curl -fsSL get.docker.com -o get-docker.sh
+      sudo sh get-docker.sh > /dev/null 2>&1
+      export DOCKER_INSTALL_RESULT=$?
+      
+      # Installing Docker Compose
+      sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose > /dev/null 2>&1
+      sudo chmod +x /usr/local/bin/docker-compose > /dev/null 2>&1
+      export DOCKER_COMPOSE_INSTALL_RESULT=$?
+   
+      # If all the setup are done then the further script will execute
+      ./setup.bash
 
-         elif [ $1 == start ]
+   fi
+
+elif [ $IS_DOCKER_COMPOSE_PRESENT -ge 1 ]
+   
+   then
+   
+   if [ $DOCKER_COMPOSE_INSTALL_RESULT -ge 1 ] > /dev/null 2>&1
+   
+   then
+   
+      echo "Script tried to install docker but failed durning installation"
+      echo "trying Installing docker and docker compose manually"
+   
+   else
+
+      echo "Looks like docker is not installed in this system"
+      echo "Installing Docker..."
+
+      sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose > /dev/null 2>&1
+      sudo chmod +x /usr/local/bin/docker-compose > /dev/null 2>&1
+      export DOCKER_COMPOSE_INSTALL_RESULT=$?
+      ./setup.bash
+
+   fi
+
+else
+
+      if [ $# -lt 1 ]
+         then
+         echo "$usage"
+
+      elif [ $1 == start ]
+         then
+         if [ $# -lt 2 ]
             then
-            if [ $# -lt 2 ]
-               then
-               echo $#
-            else
-               if [ $2 == "-d" ]
-                  then
-                  if [ $# -lt 3 ]
-                     then
-                     echo "Please provide a domain name"
-                  fi
-                  else
-                  echo "Invalid Option $2"
-               fi
-            fi
+            echo "Domain name not provided..."
+            echo "Taking default domain name 'example.com'"
+            export DOMAIN="example.com"
             docker-compose up -d
+            if [ $? -gt 0 ]
+               then
+               echo "Error Occurred"
+               else
+               echo "Open $DOMAIN in your browser"
+            fi
+         else
+            if [ $2 == "-d" ]
+               then
+               if [ $# -lt 3 ]
+                  then
+                  echo "Please provide a domain name"
+                  else
+                  export DOMAIN=$3
+                  echo "Domain name provided... $DOMAIN"
+                  docker-compose up -d
+                  if [ $? -gt 0 ]
+                     then
+                     echo "Error Occurred"
+                     else
+                     echo "Open $DOMAIN in your browser"
+                  fi
+               fi
+            else
+            echo "Invalid Option $2"
+            fi
+         fi
 
-         elif [ $1 == stop ]
-            then
-            docker-compose stop
+      elif [ $1 == stop ]
+         then
+         docker-compose stop
 
-         elif [ $1 == delete ]
-            then
-            docker-compose down
+      elif [ $1 == delete ]
+         then
+         docker-compose down
 
-         elif [ $1 == describe ]
-            then
-            docker-compose ps
+      elif [ $1 == describe ]
+         then
+         docker-compose ps
 
-      fi
+   fi
 
 fi
